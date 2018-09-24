5a1b9c2cd5327d6d896b95d949149053c70e95f1
Ba1720258 <43261464+Ba1720258@users.noreply.github.com>
2018-09-24 13:55:31 +0930

    done addDish function

--- bianca-functions.cpp
+++ bianca-functions.cpp
@@ -3,4 +3,12 @@
 #include "dish.h"
 #include "mains.h"
 #include "dessert.h"
+//#include "menu.h"
 
+//add dish function - may need to adapt for subclasses?
+void addDish(dish * order, int * currentDishes, dish addedDish, double dishCost, double * totalCost) {
+	order[currentDishes] = addedDish;
+	currentDishes++;
+	totalCost = totalCost + dishCost;
+	cout << addedDish.getName() << " successfully added to order!\n";
+}
