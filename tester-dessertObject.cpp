be463cbb1be711fcf2cecbbb2a9974d24db0ae74
Ba1720258 <43261464+Ba1720258@users.noreply.github.com>
2018-10-03 11:41:53 +0930

    replacing corrupted dessert test file

---
+++ tester-dessertObject.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "dish.h"
+#include "dessert.h"
+
+using namespace std;
+
+int main() {
+	dessert chocolateMousse = dessert("Chocolate Mousse", 8.90, "dairy, eggs", "sweet");
+
+	cout << "Dish name: " << chocolateMousse.getName() << "\n";
+	cout << "Price: $" << chocolateMousse.getPrice() << "\n";
+	cout << "It is " << chocolateMousse.howSweet() << "\n";
+	cout << "*Contains " << chocolateMousse.getInfo() << "\n";
+
+	return 0;
+}
