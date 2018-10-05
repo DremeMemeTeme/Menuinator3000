4cd281f5d398c72ec3869ee36d81fba4d7784fb7
Va1725646 <a12725646@student.adelaide.edu.au>
2018-10-05 11:44:23 +0930

    menu filess

--- menu.cpp
+++ menu.cpp
@@ -8,28 +8,41 @@
 
 using namespace std;
 
-
-
-
-
-
-
-
-
-
-
 menu::menu(){
+	 dish * dishArray;
+	 menuName = "Generic Menu Name";
+	 maxItems = 10;
+	 dishType = "Generic Food Item"; 
+
+} 
+
+menu::menu(std::string menuTitle, std::string itemType, int numOfItems)
+{
+	dish * dishArray;
+	menuName = menuTitle;
+	maxItems = 
+	dishType = itemType;
+
 }
 
+void menu::addDish(dish foodtoAdd){
+	if(dishCount < maxItems){
+		dishCollection[dishCount] = foodtoAdd;
+		dishCount++;
+	}
+}
 
+string menu::dishName(){
 
+}
 
+int menu::getMax(){
+	return maxItems;
+}
 
-
-
-
-
-
+void setName(string newName){
+	menuName = newName;
+}
 
 menu::~menu(){
 }
--- menu.h
+++ menu.h
@@ -11,24 +11,25 @@ class menu
 	//variables
 	dish * dishArray;  // holds the differnt objects, probably should make this dynamic
 	std::string menuName; 
-	int maxItems; //max number of dishes you can add
-
-
+	int maxItems; //max number of dishes you can add to menu (not order)
+	std::string dishType; 
 
 public: 
 	//constructor
 	menu();
-	menu(std::string menuTitle, int numOfItems);
+	menu(std::string menuTitle, std::string itemType, int numOfItems);
 
 	//behaviour
 	void addDish(dish food); //add main,drinks,dessert objects into menu 
 
 	//getter
 	std::string dishName();  
-	int getMax(); 
+	int getMax();  //max number of dishes you can add to menu 
+
+	//setter
+	void setName(std::string newName); //gives a menu name to main, desserts, dinks menu
 
 	~menu();
-
 
 };
 
