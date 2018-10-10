872096d71e0a7a7b679c67db103225e38898df64
Va1725646 <a12725646@student.adelaide.edu.au>
2018-10-10 12:02:38 +1030

    Menuinator main

---
+++ menuinator_main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+#include <string.h>
+#include "dish.h"
+#include "mains.h"
+#include "dessert.h"
+#include "drink.h"
+#include "menu.h"
+
+using namespace std;
+
+int main(){
+
+
+
+
+
+}
