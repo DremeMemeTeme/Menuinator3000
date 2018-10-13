f97cd5e2aff53f1ca15ab4ee7765a5810c7e3dce
Ba1720258 <43261464+Ba1720258@users.noreply.github.com>
2018-10-13 15:47:50 +1030

    worked on main program, began testing and replaced corrupted file

--- V_functions2.cpp
+++ V_functions2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "menu.h"
 using namespace std;
 
 
@@ -15,15 +16,17 @@ void displayOptions()
 
 
 //DISPLAYS MAIN MENU
-void displayMain(int mainSize, menu mainsMenu);
+void displayMain(int mainSize, menu mainsMenu)
 {
 	int size = mainSize; //size corresponds to the number of items in both menu and price array
 
 		cout << "HERE IS OUR MAINS MENU:" << '\n';
+		/*
 		for(int i=0; i<size; i++)
 		{
 			cout << menuArray[i] << "....." << "$ " << menuPriceArray[i] << '\n';
 		}	
+		*/ //commented out for testing purposes - since it's not quite finished yet!
 }
 
 
